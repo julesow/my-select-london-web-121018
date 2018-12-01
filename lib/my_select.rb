50ea781691194d794c8d634fbf11fb3ebbbd97fa
SOULEYMANE SOW <souleymanesow@gmail.com>
2018-12-01 07:44:51 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,13 @@
 def my_select(collection)
  # your code here!
+     new_array = []
+     i=0
+     while i < collection.length 
+      
+         new_array<< ( yield collection[i] )
+     
+        i += 1
+    
+    end
+    return new_array
 end
